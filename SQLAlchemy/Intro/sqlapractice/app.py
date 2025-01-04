f901404f7bcc85c997161766a7bb47b84227e316
lando924 <ojcastaneda9@gmail.com>
2025-01-03 23:58:25 -0500

    404 sqlapractice species

--- SQLAlchemy/Intro/sqlapractice/app.py
+++ SQLAlchemy/Intro/sqlapractice/app.py
@@ -20,9 +20,27 @@ def list_pets():
     pets = Pet.query.all()
     return render_template('lists.html', pets=pets)
 
+@app.route('/', methods=["POST"])
+def create_pet():
+    name = request.form["name"]
+    species = request.form["species"]
+    hunger = request.form["hunger"]
+    hunger = int(hunger) if hunger else None
+
+    new_pet = Pet(name=name, species=species, hunger=hunger)
+    db.session.add(new_pet)
+    db.session.commit()
+
+    return redirect(f"{new_pet.id}")
+
 @app.route('/<int:pet_id>')
 def show_pet(pet_id):
     """Show details about a single pet"""
-    pet = Pet.query.get(pet_id)
+    pet = Pet.query.get_or_404(pet_id)
     return render_template("details.html", pet=pet)
+
+@app.route("/species/<species_id>")
+def show_pet_by_species(species_id):
+    pets = Pet.get_by_species(species_id)
+    return render_template("species.html", pets=pets, species=species_id)
 
