83e8415be360e1aecf7b008f03162b0b83cd28a3
lando924 <ojcastaneda9@gmail.com>
2025-01-03 22:05:13 -0500

    SQLA Practice

--- SQLAlchemy/Intro/sqlapractice/app.py
+++ SQLAlchemy/Intro/sqlapractice/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_shop_db'
-
+debug = DebugToolbarExtension(app)
 
 app.config['SECRET_KEY'] = "chickenzarecool121837"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
@@ -15,6 +15,14 @@ app.app_context().push()
 connect_db(app)
 
 @app.route('/')
-def home_page():
-    """Shows home page"""
-    return render_template('home.html')
+def list_pets():
+    """Shows list pets in db"""
+    pets = Pet.query.all()
+    return render_template('lists.html', pets=pets)
+
+@app.route('/<int:pet_id>')
+def show_pet(pet_id):
+    """Show details about a single pet"""
+    pet = Pet.query.get(pet_id)
+    return render_template("details.html", pet=pet)
+
--- SQLAlchemy/Intro/sqlapractice/models.py
+++ SQLAlchemy/Intro/sqlapractice/models.py
@@ -15,6 +15,20 @@ def connect_db(app):
 class Pet(db.Model):
     __tablename__= "pets"
 
+
+    @classmethod
+    def get_by_species(cls, species):
+        cls.query.filter_by(species=species).all()
+
+    @classmethod
+    def get_all_hungry(cls):
+        return cls.query.filter(Pet.hunger > 20).all()
+
+
+    def __repr__(self):
+        p = self
+        return f"<Pet id={p.id} name={p.species} hunger={p.hunger}>"
+
     id = db.Column(db.Integer,
                    primary_key=True,
                    autoincrement=True)
@@ -25,3 +39,13 @@ class Pet(db.Model):
                         nullable=True,
                         )
     hunger = db.Column(db.Integer, nullable=False, default=20)
+
+    def greet(self):
+        return f"Hi, I am {self.name} the {self.species} "
+    
+    def feed(self, amt=20):
+        """Update hunger based off of amt"""
+        self.hunger -= amt
+        self.hunber = max(self.hunger, 0)
+
+
