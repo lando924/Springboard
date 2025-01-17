8f5e4bf6e7e502d38967353d9488fabac93af2f7
lando924 <ojcastaneda9@gmail.com>
2025-01-16 23:58:23 -0500

    WTF Debug finally working

--- IntermediateFlask/practice/app.py
+++ IntermediateFlask/practice/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
@@ -6,7 +6,9 @@ from forms import AddSnackForm
 
 
 app = Flask(__name__)
+app.env= 'development'
 app.debug = True
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask_wtforms'
 app.config["SECRET_KEY"] = "oh-so-secret"
@@ -14,6 +16,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 app.config['DEBUG_TB_PROFILER_ENABLED'] = True
 app.config['DEBUG_TB_ENABLED'] = True
+app.config['DEBUG'] = True
+
 
 
 debug = DebugToolbarExtension(app)
@@ -26,7 +30,7 @@ connect_db(app)
 @app.route("/")
 def homepage():
     """Show homepage links."""
-
+    
     return render_template("index.html")
 
 
@@ -48,12 +52,12 @@ def homepage():
 
 @app.route('/snacks/new', methods=["GET","POST"])
 def add_snack():
+    print(request.form)
     form = AddSnackForm()
     if form.validate_on_submit():
-        raise
-        print(form.name.data)
-        print(form.price.data)
-        flash("Snack added successfully!")
+        name = form.name.data
+        price = form.price.data
+        flash(f"Created new snack: name is {name}, price is ${price}!")
         return redirect("/")
     else: 
         return render_template("add_snack_form.html", form=form)
